be98f7be4da65e5ad0d7cea8e99975a2d44cff6a
mohamed-295 <ma2267204@gmail.com>
2025-05-04 01:30:35 +0300

    Update Constants

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -11,5 +11,5 @@ class AppConstants {
   static String removeSpecificCartEndPoint(String id) => 'cart/$id';
   static String deleteUserCartEndPoint = 'cart';
   static const String updateProfileEndPoint = 'users/updateMe';
-  static const String getProfileEndPoint = 'users/me';
+
 }
